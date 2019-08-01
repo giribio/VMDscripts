c8e6a92d3e46d3c2535e3ce9274d2098e490cf0f
Lazemare <zjt033@gmail.com>
2019-08-02 01:12:23 +0800

    Update align.tcl

--- align.tcl
+++ align.tcl
@@ -14,9 +14,9 @@ set f0 [atomselect top "$align_ref" frame $align_frame]
 set f [atomselect top "$align_ref"]
 set sel [atomselect top "$align_selection"]
 for { set i 1 } { $i < $nf } { incr i } {
-$f frame $i
-$sel frame $i
-set mv [measure fit $f $f0]
-$sel move $mv
+    $f frame $i
+    $sel frame $i
+    set mv [measure fit $f $f0]
+    $sel move $mv
 }
 
