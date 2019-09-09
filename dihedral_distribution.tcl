d38b384357b4414e40882edf9a1ff1133271e786
Lazemare <zjt033@gmail.com>
2019-09-10 00:25:14 +0800

    Add files via upload

---
+++ dihedral_distribution.tcl
@@ -0,0 +1,57 @@
+# Written by Lazemare.
+# Used to calculate the distribution alone the simulation trajectory of the 
+# dihedral between two planes, which are defined by four points, and give out 
+# the Distribution Percentage. Note that these two planes are defined separately 
+# by the position of select1, select2, select3 and select2, select3, select4.
+#--------------------------------------------------- 
+# Here are the paras:
+set outfile [open dihedral_distribution.dat w]
+set select1 "index 0"
+set select2 "index 4"
+set select3 "index 7"
+set select4 "index 10"
+#---------------------------------------------------
+set nf [molinfo top get numframes]
+set sel1 [atomselect top "$select1"]
+set sel2 [atomselect top "$select2"]
+set sel3 [atomselect top "$select3"]
+set sel4 [atomselect top "$select4"]
+for { set i 0 } { $i < 180 } { incr i } {    
+	set density($i) 0.0
+}
+for { set i 1 } { $i <= $nf } { incr i } {      
+	$sel1 frame $i
+	set V1 [measure center "$sel1"]
+	$sel2 frame $i
+	set V2 [measure center "$sel2"]
+	$sel3 frame $i
+	set V3 [measure center "$sel3"]
+	$sel4 frame $i
+	set V4 [measure center "$sel4"]
+	set V1 [vecsub $V1 $V2]
+	set V2 [vecsub $V3 $V2]
+	set V3 [vecsub $V4 $V3]
+	set norm1 [veccross $V1 $V2] 
+	set norm2 [veccross $V2 $V3]
+	set norm1 [vecscale [expr 1 / [veclength $norm1]] $norm1]
+	set norm2 [vecscale [expr 1 / [veclength $norm2]] $norm2]
+	set m [veccross $norm1 [vecscale [expr 1 / [veclength $V2]] $V2]]
+	set x [vecdot $norm1 $norm2]
+	set y [vecdot $m $norm2]
+	set DIHED [expr atan2($y,$x)*180/3.1415926] 
+	if {$DIHED > 0} {
+		set DIHED [expr 180.0 - $DIHED]
+	} else {
+		set DIHED [expr -180.0 - $DIHED]
+	}
+	set index [expr int(floor([expr ($DIHED + 180) / 2]))]
+	set density($index) [expr $density($index) + 1.0]
+}
+for { set i 1 } { $i < 180 } { incr i } {    
+	set density($i) [expr $density($i) / $nf * 100]
+	puts -nonewline $outfile "[expr $i * 2 - 1 - 180 ]"
+	puts -nonewline $outfile " "
+	puts $outfile "[expr $density($i)]"
+}
+close $outfile
+puts "All Done!"
